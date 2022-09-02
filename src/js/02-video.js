cf321c571ec3dedb9872992fb3539c21552ed22f
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-09-03 00:31:01 +0300

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', onPlay);
+
+localStorage.setItem('сообщение', 'привет');
