a89dfe30942697ebdecdd3cb81ee1cef46c5c6e2
AlonaStasyshyna <95381227+AlonaStasyshyna@users.noreply.github.com>
2022-09-04 21:16:41 +0000

    Deploying to gh-pages from @ AlonaStasyshyna/goit-js-hw-08@1e9c7bd914d0e6ec3f6381f988624dbbdb4c016b 🚀

--- 03-feedback.e4dd1d71.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form"),document.querySelector('input[name="email"]');document.querySelector('textarea[name="message"]').addEventListener("input",(()=>{})),localStorage.setItem("message","ghbdt");
-//# sourceMappingURL=03-feedback.e4dd1d71.js.map
--- 03-feedback.f4db6767.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".feedback-form"),document.querySelector('input[name="email"]');document.querySelector('textarea[name="message"]').addEventListener("input",(function(){})),localStorage.setItem("message","ghbdt")}();
-//# sourceMappingURL=03-feedback.f4db6767.js.map
